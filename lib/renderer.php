6f2da0207236f8b2aa0e0064c168ce0edda6541a
Aleksi Peebles <aleksi@iki.fi>
2020-06-25 23:00:37 +0300

    Render template strings instead of files

--- lib/renderer.php
+++ lib/renderer.php
@@ -3,9 +3,14 @@ $autoload = dirname(__DIR__) . '/vendor/autoload.php';
 if (file_exists($autoload)) {
   include $autoload;
 }
-$options = getopt(null, ['file:', 'data::']);
+$options = getopt(null, ['string::', 'file::', 'data::']);
 if (!empty($options['data'])) {
   $data = json_decode(base64_decode($options['data']), true);
   extract($data);
 }
-include $options['file'];
+if (!empty($options['string'])) {
+  eval('?>' . base64_decode($options['string']));
+}
+elseif (!empty($options['file'])) {
+  include $options['file'];
+}
