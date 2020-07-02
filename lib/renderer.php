6e734ec842bb87501c1efd7983fdd739f25eb533
Aleksi Peebles <aleksi.peebles@helsinki.fi>
2020-07-02 18:11:26 +0300

    Add initial render method support

--- lib/renderer.php
+++ lib/renderer.php
@@ -1,16 +1,31 @@
 <?php
+class Renderer
+{
+    public function run($options)
+    {
+        if (!empty($options['data'])) {
+            $data = json_decode(base64_decode($options['data']), true);
+            extract($data);
+        }
+        if (!empty($options['string'])) {
+            eval('?>' . base64_decode($options['string']));
+        } elseif (!empty($options['file'])) {
+            include $options['file'];
+        }
+    }
+
+    public function render($name, $values = null)
+    {
+        $patternsPath = dirname(dirname(dirname(dirname(__DIR__)))) . '/source/';
+        if (!empty($values)) {
+            extract($values);
+        }
+        include "$patternsPath/$name";
+    }
+}
 $autoload = dirname(__DIR__) . '/vendor/autoload.php';
 if (file_exists($autoload)) {
-  include $autoload;
+    include $autoload;
 }
-$options = getopt(null, ['string::', 'file::', 'data::']);
-if (!empty($options['data'])) {
-  $data = json_decode(base64_decode($options['data']), true);
-  extract($data);
-}
-if (!empty($options['string'])) {
-  eval('?>' . base64_decode($options['string']));
-}
-elseif (!empty($options['file'])) {
-  include $options['file'];
-}
+$renderer = new Renderer();
+$renderer->run(getopt(null, ['string::', 'file::', 'data::']));
